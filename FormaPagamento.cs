5c9ec24d30280712386ee6f9a589ad891129952d
RafaelMorais <rafaelmorais.design@gmail.com>
2023-01-12 20:32:34 -0300

    Exercício usando Enum, Polimorfismo e Interface

---
+++ FormaPagamento.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioInterface
+{
+    public class FormaPagamento
+    {
+        public virtual void EfetuarPagamento() 
+        {
+            Console.WriteLine("Pagamento efetuado");
+        }
+    }
+
+    public class Boleto : FormaPagamento
+    {
+        public override void EfetuarPagamento()
+        {
+            Console.WriteLine("Pagamento feito com boleto");
+        }
+    }
+
+    public class Pix : FormaPagamento
+    {
+        public override void EfetuarPagamento()
+        {
+            Console.WriteLine("Pagamento feito com Pix");
+        }
+    }
+
+    public class Cartao : FormaPagamento
+    {
+        public override void EfetuarPagamento()
+        {
+            Console.WriteLine("Pagamento feito com cartão");
+        }
+    }
+
+    public class Dinheiro : FormaPagamento
+    {
+        public Dinheiro()
+        {
+            EfetuarPagamento();
+        }
+        public override void EfetuarPagamento()
+        {
+            Console.WriteLine("Pagamento feito com dinheiro");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿/* Escrever um programa que recebe os produtos a serem comprados
+ * Escolher a forma de pagamento
+ * De acordo com a forma de pagamento escolhida, efetuar compra*/
+
+using DesafioInterface;
+
+List<string> listaProdutos = new List<string>();
+
+Console.WriteLine("Digite três produtos a serem comprados:");
+
+for(int i = 0; i < 3; i++)
+{
+    listaProdutos.Add(Console.ReadLine());
+}
+
+Console.WriteLine("Qual forma de pagamento você deseja? (Boleto, Cartão, Pix, Dinheiro)");
+
+string formaDePagamento = Console.ReadLine();
+
+switch (formaDePagamento)
+{
+    case "Boleto":
+        Boleto boleto = new Boleto();
+        boleto.EfetuarPagamento();
+        break;
+    case "Cartão":
+        Cartao cartao = new Cartao();
+        cartao.EfetuarPagamento();
+        break;
+    case "Pix":
+        Pix pix = new Pix();
+        pix.EfetuarPagamento();
+        break;
+    case "Dinheiro":
+        Dinheiro dinheiro = new Dinheiro();
+        break;
+    default:
+        Console.WriteLine("Método de pagamento não encontrado");
+        break;
+}
